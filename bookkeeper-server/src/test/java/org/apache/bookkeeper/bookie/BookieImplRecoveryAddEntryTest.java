1ea8c13d4b5c310b53680de4addae85da264c91c
Brauzi35 <57174753+Brauzi35@users.noreply.github.com>
2023-11-23 11:36:58 +0100

    BookieImplRecoveryAddEntryTest is now parametrized with sufficient results

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/BookieImplRecoveryAddEntryTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/BookieImplRecoveryAddEntryTest.java
@@ -14,6 +14,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -24,58 +25,122 @@ import static org.junit.Assert.*;
 @RunWith(Parameterized.class)
 public class BookieImplRecoveryAddEntryTest {
 
+    enum Objects{
+        VALID,
+        INVALID,
+
+    }
 
     private static BookieImpl bookieimpl;
     private static AtomicBoolean flag = new AtomicBoolean(false);
+
+    private ByteBuf bb;
+    private WriteCallback cb;
+    private Object ctx;
+    private byte[] mk;
+    private boolean expExc;
 
 
     @Parameters
     public static Collection<Object[]> getParameters() {
         // Creazione dell'oggetto ByteBuf con le stesse proprietà
         return Arrays.asList(new Object[][]{
+                //entry, cb, ctx, masterKey, expExeption
+
+                {buildEntry(Objects.VALID, 1), getCallback(Objects.VALID), "ctx", "key".getBytes(), false},
+                {buildEntry(Objects.INVALID, 1), getCallback(Objects.VALID), "ctx", "key".getBytes(), true},
+                {buildEntry(Objects.VALID, 1), getCallback(Objects.INVALID), "ctx", "key".getBytes(), true},
+                {buildEntry(Objects.VALID, 1), getCallback(Objects.VALID), null, "key".getBytes(), true},
+                {buildEntry(Objects.VALID, 1), getCallback(Objects.VALID), "ctx", "".getBytes(), false},
+                {buildEntry(Objects.VALID, 1), getCallback(Objects.VALID), "ctx", null, true}
+
 
 
         });
     }
 
-    private static WriteCallback getCallback() {
+    private static WriteCallback getCallback(Objects ob) {
 
-        return (rc, ledgerId, entryId, addr, ctx) -> flag.set(rc == BKException.Code.OK);
+        switch (ob){
+            case VALID:
+                return (rc, ledgerId, entryId, addr, ctx) -> flag.set(rc == BKException.Code.OK);
+            case INVALID:
+                return (rc, ledgerId, entryId, addr, ctx) -> {
+                    throw new RuntimeException("Invalid callback");
+                };
+            default:
+                Assert.fail("something went wrong in getCallBack");
+                return null;
+        }
+
     }
 
 
-    private static ByteBuf buildEntry(long ledger) {
-        byte[] data = ("ledger-" + ledger + "-" + 1).getBytes();
-        ByteBuf bb = Unpooled.buffer(8 + 8 + data.length);
-        bb.writeLong(ledger);
-        bb.writeLong(1);
-        bb.writeBytes(data);
-        return bb;
+    private static ByteBuf buildEntry(Objects ob, long ledger) {
+        switch (ob) {
+            case VALID:
+                byte[] data = ("ledger-" + ledger + "-" + 1).getBytes();
+                ByteBuf bb = Unpooled.buffer(8 + 8 + data.length);
+                bb.writeLong(ledger);
+                bb.writeLong(1);
+                bb.writeBytes(data);
+                return bb;
+            case INVALID:
+                ByteBuf ibb = Mockito.mock(ByteBuf.class);
+                //ByteBuf exixts to collect bytes, so i want to throw an exception anytime those bytes are accessed
+                Mockito.doThrow(new RuntimeException("invalid ByteBuf")).when(ibb).getByte(Mockito.anyInt());
+                return ibb;
+            default:
+                Assert.fail("something went wrong in buildEntry");
+                return null;
+        }
     }
 
-    public BookieImplRecoveryAddEntryTest() throws Exception {
+    private static void init() {
         //create bookieImpl using existing setup classes from bookkeeper apache
-        ServerConfiguration conf = TestBKConfiguration.newServerConfiguration();
-        bookieimpl = new BookieImplSetup(conf);
-        bookieimpl.start();
-        //TO-DO implement try-catch to correctly catch any undesired behaviour
+        try {
+            ServerConfiguration conf = TestBKConfiguration.newServerConfiguration();
+            bookieimpl = new BookieImplSetup(conf);
+            bookieimpl.start();
+        } catch (Exception e) {
+            Assert.fail("undesired exception in setup operations, something went wrong");
+            e.printStackTrace();
+        }
     }
+
+    public BookieImplRecoveryAddEntryTest(ByteBuf bb, WriteCallback cb, Object ctx, byte[] mk, boolean expExc){
+        init();
+
+        this.bb = bb;
+        this.cb = cb;
+        this.ctx = ctx;
+        this.mk = mk;
+        this.expExc = expExc;
+
+    }
+
+
+
 
     @Test()
     public void recoveryAddEntry(){
 
         boolean isCorrect = true;
-        try {
-            bookieimpl.recoveryAddEntry(buildEntry(1), getCallback(), "test", "ciao".getBytes());
-        } catch (IOException e) {
-            isCorrect = false;
-        } catch (BookieException e) {
-            isCorrect = false;
-        } catch (InterruptedException e) {
-            isCorrect = false;
-        }
 
-        Assert.assertTrue(isCorrect);
+        if (!this.expExc){
+            try {
+                bookieimpl.recoveryAddEntry(this.bb, this.cb, this.ctx, this.mk);
+            } catch (IOException | InterruptedException | BookieException e) {
+                if(!expExc){
+                    //if I expect no exception to be raised, but I've caught one, there is an
+                    //underside behaviour -> the test should fail
+                    isCorrect = false;
+                }
+
+            }
+            Assert.assertTrue(isCorrect);
+
+        }
 
     }
 
